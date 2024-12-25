2075cbdc9923e6a250dd0011357eac8fb9ab9311
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-12-25 14:02:23 +0100

    initial setup

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+const root = document.getElementById('root');
+
+if ( root )
+{
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+else
+{
+    console.error(`Fatal bug: could not find element with id="root"`);
+}
