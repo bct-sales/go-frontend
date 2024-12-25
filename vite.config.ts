d7d6ba995168efdafde92afd6edc99460f00d085
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-12-25 15:52:44 +0100

    closes #3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+
+interface Definitions
+{
+    ROOT_URL : string;
+}
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command } ) => {
+    // command === 'serve' on npm run dev
+    // command === 'build' on npm run build
+    const isDevBuild = command === 'serve';
+
+    return {
+        plugins: [
+            react(),
+            tsconfigPaths()
+        ],
+        define: buildSpecificationDefinitions(),
+    };
+
+
+    function devBuildDefinitions() : Definitions
+    {
+        return {
+            ROOT_URL: JSON.stringify('http://localhost:8000/api/v1'),
+        };
+    }
+
+    function prodBuildDefinitions() : Definitions
+    {
+        return {
+            ROOT_URL: JSON.stringify('/api/v1'),
+        };
+    }
+
+    function buildSpecificationDefinitions() : Definitions
+    {
+        if ( isDevBuild )
+        {
+            return devBuildDefinitions();
+        }
+        else
+        {
+            return prodBuildDefinitions();
+        }
+    }
 })
