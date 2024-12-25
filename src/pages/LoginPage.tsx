4736ee9e4151e9d1dd21fcf0e6a31e2168f6f5c7
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-12-25 23:30:49 +0100

    login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,122 @@
+import * as rest from '@/rest/login';
+import { Box, Button, Center, Group, Modal, PasswordInput, Text, TextInput, Title } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 
-export default function LoginPage() : React.ReactNode
+interface FormFields
 {
+    userId: string;
+    password: string;
+}
+
+export default function LoginPage()
+{
+    const navigate = useNavigate();
+    const form = useForm<FormFields>({
+        initialValues: {
+            userId: '',
+            password: ''
+        },
+        validate: {
+            userId: login => isValidLogin(login) ? null : "Enter a series of digits",
+            password: password => (isValidPassword(password) ? null : "Enter a password"),
+        }
+    });
+
+    // React.useEffect(() => {
+    //     if ( auth.isAuthenticated() )
+    //     {
+    //         navigate('/');
+    //     }
+    // });
+
     return (
-        <p>
-            Login!
-        </p>
+        <>
+            <Center mih='50vh'>
+                <Box maw={500} mx="auto" w='40%'>
+                    <form onSubmit={form.onSubmit(onSubmit)}>
+                        <Center>
+                            <Box miw='20em'>
+                                {renderLoginInput()}
+                                {renderPasswordInput()}
+                            </Box>
+                        </Center>
+
+                        <Button type="submit">Login</Button>
+                    </form>
+                </Box>
+            </Center>
+        </>
     );
+
+
+    function renderLoginInput(): React.ReactNode
+    {
+        return (
+            <TextInput label="Your ID" placeholder="Number" {...form.getInputProps('userId')} p='sm' />
+        );
+    }
+
+    function renderPasswordInput(): React.ReactNode
+    {
+        return (
+            <PasswordInput label="Password" placeholder="Password" {...form.getInputProps('password')} p='sm' />
+        );
+    }
+
+    function isValidLogin(login: string): boolean
+    {
+        return /^\d+$/.test(login);
+    }
+
+    function isValidPassword(password: string): boolean
+    {
+        return password.length > 0;
+    }
+
+    function onSubmit(formFields: FormFields)
+    {
+        void (async () => {
+            const authenticationParameters: rest.LoginParameters = {
+                userId: parseInt(formFields.userId),
+                password: formFields.password,
+            };
+
+            const result = await rest.login(authenticationParameters);
+
+            if ( result )
+            {
+                console.log('Login successful');
+            }
+            else
+            {
+                console.log('Login failed');
+            }
+
+            // if ( result.success )
+            // {
+            //     const { userId, role, accessToken } = result.value;
+
+            //     if ( !auth.isAuthenticated() )
+            //     {
+            //         auth.login({ role, accessToken, userId });
+
+            //         navigate("/");
+            //     }
+            //     else
+            //     {
+            //         console.error('Bug detected: user should not be able to reach login page while authenticated');
+            //         setMessage('You have encountered a bug');
+            //         openMessageBox();
+            //     }
+            // }
+            // else
+            // {
+            //     setMessage(result.error);
+            //     openMessageBox();
+            // }
+        })();
+    }
 }
