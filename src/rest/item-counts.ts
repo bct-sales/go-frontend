7c7a6e1e0f6d8fd51778330c7df379795907dd9e
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-12-28 00:32:53 +0100

    rest item counts

---
+++ src/rest/item-counts.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { z } from 'zod';
+import { extractDetailFromException } from './errors';
+
+
+
+const ItemCountsResponse = z.record(z.number());
+
+type ItemCountsResponse = z.infer<typeof ItemCountsResponse>;
+
+
+export async function listUsers(): Promise<ItemCountsResponse | undefined>
+{
+    const url = `${ROOT_URL}/category-counts`;
+
+    try
+    {
+        const response = await axios.get<unknown>(url);
+        const data = ItemCountsResponse.parse(response.data);
+
+        return data;
+    }
+    catch ( error: unknown )
+    {
+        const detail = extractDetailFromException(error);
+
+        if ( detail !== null )
+        {
+            return undefined;
+        }
+        else
+        {
+            console.error(error);
+            return undefined;
+        }
+    }
+}
