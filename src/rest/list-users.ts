10ff53d2e8d876d02bedd6e2588937717b9dfbf9
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-12-27 16:25:20 +0100

    listUsers

---
+++ src/rest/list-users.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import { z } from 'zod';
+import { extractDetailFromException } from './errors';
+
+
+
+const User = z.object({
+    id: z.number(),
+    password: z.string(),
+    role: z.union([z.literal('seller'), z.literal('admin'), z.literal('cashier')]),
+    created_at: z.object({
+        year: z.number(),
+        month: z.number(),
+        day: z.number(),
+        hour: z.number(),
+        minute: z.number(),
+        second: z.number(),
+    }),
+});
+
+export type User = z.infer<typeof User>;
+
+
+const ListUsersResponse = z.object({
+    users: z.array(User),
+})
+
+type ListUsersResponse = z.infer<typeof ListUsersResponse>;
+
+
+export async function listUsers(): Promise<User[] | undefined>
+{
+    const url = `${ROOT_URL}/users`;
+
+    try
+    {
+        const response = await axios.get<unknown>(url);
+        const data = ListUsersResponse.parse(response.data);
+
+        return data.users;
+    }
+    catch ( error: unknown )
+    {
+        const detail = extractDetailFromException(error);
+
+        if ( detail !== null )
+        {
+            return undefined;
+        }
+        else
+        {
+            console.error(error);
+            return undefined;
+        }
+    }
+}
