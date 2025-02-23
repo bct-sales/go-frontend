9accd40ad3248b91da2a241addac927d17a1b0de
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-23 16:41:10 +0100

    improved extractDetailFromException

--- src/rest/errors.ts
+++ src/rest/errors.ts
@@ -1,17 +1,33 @@
 import axios from 'axios';
 
 
-export function extractDetailFromException(error: unknown): string | null
+export interface Parser<T>
+{
+    safeParse: (data: unknown) => { success: true, data: T } | { success: false, error: unknown };
+}
+
+export function extractDetailFromException<T>(error: unknown, parser : Parser<T>, unknownError : T): T
 {
     if ( axios.isAxiosError(error) && error.response )
     {
         const data = error.response.data as unknown;
+        const parsed = parser.safeParse(data);
 
-        if ( typeof data === 'object' && data !== null && 'detail' in data && typeof data.detail === 'string' )
+        if ( parsed.success )
         {
-            return data.detail;
+            return parsed.data;
+        }
+        else
+        {
+            console.error("Failed to parse error", parsed.error);
+            return unknownError;
         }
     }
 
-    return null;
+    return unknownError;
+}
+
+export function createErrorExtractor<T>(parser : Parser<T>, unknownError : T) : (error : unknown) => T
+{
+    return (error: unknown) => extractDetailFromException(error, parser, unknownError);
 }
