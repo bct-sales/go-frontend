541a4906d4ac8c6f13a6c524ac57f2d54025ccfc
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-04-25 11:19:11 +0200

    listSellerItems

---
+++ src/rest/list-seller-items.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { z } from 'zod';
+import { convertExceptionToFailure, RestResult } from '@/rest/result';
+import { paths } from '@/rest/paths';
+import { DateTime } from '@/datetime';
+import { success } from '@/result';
+
+
+const Item = z.object({
+	itemId: z.number(),
+	addedAt: DateTime,
+	description: z.string(),
+	priceInCents: z.number(),
+	categoryId: z.number(),
+	sellerId: z.number(),
+	donation: z.boolean(),
+	charity: z.boolean(),
+    frozen: z.boolean(),
+});
+
+export type Item = z.infer<typeof Item>;
+
+
+const SuccessResponse = z.object({
+    items: z.array(Item),
+})
+
+type SuccessResponse = z.infer<typeof SuccessResponse>;
+
+
+export async function listSellerItems(sellerId: number): Promise<RestResult<SuccessResponse>>
+{
+    const url = paths.sellerItems(sellerId);
+
+    try
+    {
+        const response = await axios.get<unknown>(url);
+        console.log("Seller items response:", response.data);
+        const data = SuccessResponse.parse(response.data);
+
+        return success({ items: data.items });
+    }
+    catch ( exception: unknown )
+    {
+        console.error(exception);
+        return convertExceptionToFailure(exception);
+    }
+}
