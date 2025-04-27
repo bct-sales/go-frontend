56e34bb74a7f7f63fd61b47bf5277a3774d2dcf9
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-04-27 18:31:09 +0200

    improved header in SellerDashboard

--- src/pages/SellerDashboard.tsx
+++ src/pages/SellerDashboard.tsx
@@ -1,20 +1,36 @@
-import { ActionIcon, AppShell, Flex } from "@mantine/core";
+import { ActionIcon, AppShell, Flex, Stack, Text } from "@mantine/core";
 import { IconList, IconLogout } from "@tabler/icons-react";
 import React from "react";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import ItemsSubpage from "./seller/ItemsSubpage";
+import { useAuthentication } from "@/authentication";
+import classes from './AdminDashboard.module.css';
+import AuthenticationViewer from "@/components/AuthenticationViewer";
 
 
 export default function SellerDashboard()
 {
+    const authentication = useAuthentication();
     const location = useLocation();
     const navigate = useNavigate();
 
+    if ( authentication.status !== 'authenticated' || authentication.role !== 'seller' )
+    {
+        return (
+            <Stack justify="center" align="center" style={{height: '100vh'}}>
+                <h1>Lost authentication data!</h1>
+            </Stack>
+        );
+    }
+
     return (
         <>
-            <AppShell navbar={{width: 100, breakpoint: 'sm', collapsed: {mobile: true}}} header={{height: 60}}>
-                <AppShell.Header>
-                    Seller Dashboard
+            <AppShell navbar={{width: 100, breakpoint: 'sm', collapsed: {mobile: true}}} header={{height: 100}}>
+            <AppShell.Header>
+                    <Flex direction="row" align="center" justify="space-between" gap="md" p="xl" style={{height: '100%'}}>
+                        <Text className={classes.header}>Seller Dashboard</Text>
+                        <AuthenticationViewer username={authentication.username} role={authentication.role} />
+                    </Flex>
                 </AppShell.Header>
                 <AppShell.Navbar>
                     <Flex direction="column" align="center" justify="flex-start" gap="md" m="lg" style={{height: '100%'}}>
