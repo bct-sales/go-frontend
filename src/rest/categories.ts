f21432e18c5f78ea7cc52e4b8a4b299c96d9c585
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-04-28 16:23:45 +0200

    getItemCategories

---
+++ src/rest/categories.ts
@@ -0,0 +1,37 @@
+import { success } from '@/result';
+import axios from 'axios';
+import { z } from 'zod';
+import { paths } from './paths';
+import { convertExceptionToFailure, RestResult } from './result';
+
+
+const Category = z.object({
+    categoryId: z.number().int().nonnegative(),
+    categoryName: z.string().nonempty(),
+})
+
+export type Category = z.infer<typeof Category>;
+
+const SuccessResponse = z.object({
+    categories: z.array(Category),
+});
+
+type SuccessResponse = z.infer<typeof SuccessResponse>;
+
+
+export async function getItemCategories(): Promise<RestResult<SuccessResponse>>
+{
+    const url = paths.categories;
+
+    try
+    {
+        const response = await axios.get<unknown>(url);
+        const data = SuccessResponse.parse(response.data);
+
+        return success(data);
+    }
+    catch ( error: unknown )
+    {
+        return convertExceptionToFailure(error);
+    }
+}
