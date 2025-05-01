56e1ffeacded34330197fd88c409d3f2e6d31a33
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-01 19:41:48 +0200

    add item button validation check

--- src/pages/seller/AddItemSubpage.tsx
+++ src/pages/seller/AddItemSubpage.tsx
@@ -3,7 +3,8 @@ import DonationEditor from "@/components/DonationEditor";
 import ItemCategoryEditor from "@/components/ItemCategoryEditor";
 import ItemDescriptionEditor from "@/components/ItemDescriptionEditor";
 import ItemPriceEditor from "@/components/ItemPriceEditor";
-import { Flex } from "@mantine/core";
+import { validateDescription, validatePrice } from "@/validation";
+import { Button, Flex } from "@mantine/core";
 import { useState } from "react";
 
 
@@ -14,14 +15,47 @@ export default function AddItemSubpage() : React.ReactNode
     const [categoryId, setCategoryId] = useState<number | undefined>(undefined);
     const [charity, setCharity] = useState<boolean>(false);
     const [donation, setDonation] = useState<boolean>(false);
+    const isValidData = checkValidity();
 
     return (
-        <Flex direction="column" align="stretch" justify="center" gap="md" style={{height: '100%'}}>
+        <Flex direction="column" align="stretch" justify="center" gap='md' w='400px'>
             <ItemDescriptionEditor description={description} setDescription={setDescription} />
-            <ItemPriceEditor priceInCents={priceInCents} setPriceInCents={setPriceInCents} />
+            <ItemPriceEditor priceInCents={priceInCents} setPriceInCents={setPriceInCents} quickButtons={[100, 200, 500, 1000]} />
             <ItemCategoryEditor categoryId={categoryId} setCategoryId={setCategoryId} />
             <CharityEditor charity={charity} setCharity={setCharity} />
             <DonationEditor donation={donation} setDonation={setDonation} />
+            <Button mt='xl' onClick={addItem} disabled={!isValidData}>Add Item</Button>
         </Flex>
     );
+
+
+    function addItem(): void
+    {
+        console.log('Adding item with the following details:');
+        console.log('Description:', description);
+        console.log('Price in cents:', priceInCents);
+        console.log('Category ID:', categoryId);
+        console.log('Charity:', charity);
+        console.log('Donation:', donation);
+    }
+
+    function checkValidity(): boolean
+    {
+        if ( !validateDescription(description).isValid )
+        {
+            return false;
+        }
+
+        if ( !validatePrice(priceInCents).isValid )
+        {
+            return false;
+        }
+
+        if ( categoryId === undefined )
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
