697d61bc045772a3f4b12fb2f28271ffcb71cb19
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-01 17:38:28 +0200

    DonationEditor

---
+++ src/components/DonationEditor.tsx
@@ -0,0 +1,22 @@
+import { Checkbox } from "@mantine/core";
+import DonationCheckboxIcon from "./DonationCheckboxIcon";
+
+
+interface Props
+{
+    donation: boolean;
+    setDonation: (donation: boolean) => void;
+}
+
+export default function DonationEditor(props: Props): React.ReactNode
+{
+    return (
+        <Checkbox icon={DonationCheckboxIcon} checked={props.donation} onChange={onChange} label="Donation" />
+    );
+
+
+    function onChange(event: React.ChangeEvent<HTMLInputElement>): void
+    {
+        props.setDonation(event.currentTarget.checked);
+    }
+}
