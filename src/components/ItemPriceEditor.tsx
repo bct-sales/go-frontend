2e411c11b05b175d15e4347c2a1ccd2904b14a6d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-01 18:29:11 +0200

    quick buttons in price editor

--- src/components/ItemPriceEditor.tsx
+++ src/components/ItemPriceEditor.tsx
@@ -1,11 +1,13 @@
-import { NumberInput } from "@mantine/core";
+import { Button, Flex, NumberInput } from "@mantine/core";
 import { NumberFormatValues } from "react-number-format";
+import Price from "./Price";
 
 
 interface Props
 {
     priceInCents: number;
     setPriceInCents: (priceInCents: number) => void;
+    quickButtons?: number[];
 }
 
 export default function ItemPriceEditor(props: Props): React.ReactNode
@@ -13,19 +15,50 @@ export default function ItemPriceEditor(props: Props): React.ReactNode
     const { priceInCents, setPriceInCents } = props;
 
     return (
-        <NumberInput
-            label="Price"
-            value={priceInCents / 100}
-            step={0.50}
-            min={0.50}
-            prefix='€'
-            decimalScale={2}
-            fixedDecimalScale={true}
-            decimalSeparator="."
-            isAllowed={isAllowed}
-            onChange={onChange} />
+        <Flex align="stretch" justify="flex-start" direction="column" gap="md">
+            <NumberInput
+                label="Price"
+                value={priceInCents / 100}
+                step={0.50}
+                min={0.50}
+                prefix='€'
+                decimalScale={2}
+                fixedDecimalScale={true}
+                decimalSeparator="."
+                isAllowed={isAllowed}
+                onChange={onChange} />
+            {renderQuickButtons()}
+        </Flex>
     );
 
+
+    function renderQuickButtons(): React.ReactNode
+    {
+        if ( props.quickButtons && props.quickButtons.length > 0 )
+        {
+            return (
+                <Flex direction="row" justify="flex-end" align="center" gap="md">
+                    {props.quickButtons.map(value => renderQuickButton(value))}
+                </Flex>
+            );
+        }
+        else
+        {
+            return <></>;
+        }
+    }
+
+    function renderQuickButton(priceInCents: number): React.ReactNode
+    {
+        const euros = Math.floor(priceInCents / 100);
+        const cents = priceInCents % 100;
+
+        return (
+            <Button onClick={() => setPriceInCents(priceInCents)}>
+                {`€${euros}.${cents.toString().padStart(2, '0')}`}
+            </Button>
+        );
+    }
 
     function isAllowed(values: NumberFormatValues): boolean
     {
