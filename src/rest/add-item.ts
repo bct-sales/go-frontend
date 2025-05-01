9b9abf6d4cdd92f9ce6f9b3673ae33ed0f988d69
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-01 20:34:48 +0200

    rest addItem

---
+++ src/rest/add-item.ts
@@ -0,0 +1,42 @@
+import { success } from '@/result';
+import { Role } from '@/role';
+import axios from 'axios';
+import { z } from 'zod';
+import { paths } from './paths';
+import { convertExceptionToFailure, RestResult } from './result';
+
+
+const Payload = z.object({
+    priceInCents: z.number().int().nonnegative(),
+    description: z.string().nonempty(),
+    categoryId: z.number().int().nonnegative(),
+    donation: z.boolean(),
+    charity: z.boolean(),
+}).strict();
+
+export type Payload = z.infer<typeof Payload>;
+
+
+const SuccessResponse = z.object({
+    itemId: z.number().int().nonnegative(),
+}).strict();
+
+type SuccessResponse = z.infer<typeof SuccessResponse>;
+
+
+export async function addItem(sellerId: number, payload: Payload ): Promise<RestResult<number>>
+{
+    const url = paths.sellerItems(sellerId);
+
+    try
+    {
+        const response = await axios.post<unknown>( url, payload );
+        const data = SuccessResponse.parse(response.data);
+
+        return success(data.itemId);
+    }
+    catch ( exception: unknown )
+    {
+        return convertExceptionToFailure(exception);
+    }
+}
