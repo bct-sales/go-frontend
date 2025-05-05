6c1ad17697798507c283650af891be50a9117348
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-05 15:55:11 +0200

    updated index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
 export { default as LoginPage } from './LoginPage.tsx';
 export { default as LogoutPage } from './LogoutPage.tsx';
-export { default as AdminDashboard } from './AdminDashboard.tsx';
-export { default as SellerDashboard } from './SellerDashboard.tsx';
+export { default as AdminDashboard } from './dashboards/AdminDashboard.tsx';
+export { default as SellerDashboard } from './dashboards/SellerDashboard.tsx';
