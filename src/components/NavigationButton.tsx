e0504e1f4330f3979f1624230e60e60848a97de2
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-05 21:28:22 +0200

    improved NavigationButton

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -7,18 +7,27 @@ interface Props
     caption: string;
     url: string;
     children: React.ReactNode;
+    onlyShowWhenActive?: boolean;
 }
 
 export default function NavigationButton(props: Props): React.ReactNode
 {
     const navigate = useNavigate();
     const isActive = props.url === location.pathname;
+    const isHidden = props.onlyShowWhenActive && !isActive;
 
-    return (
-        <ActionIcon onClick={onClick} title={props.caption} size={64} variant={isActive ? 'filled' : 'light'}>
-            {props.children}
-        </ActionIcon>
-    );
+    if ( isHidden )
+    {
+        return <></>;
+    }
+    else
+    {
+        return (
+            <ActionIcon onClick={onClick} title={props.caption} size={64} variant={isActive ? 'filled' : 'light'}>
+                {props.children}
+            </ActionIcon>
+        );
+    }
 
 
     function onClick(): void
