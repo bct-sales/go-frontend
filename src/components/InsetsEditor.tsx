cb75828ed5f78a46a138e18d4c7f90afc0cec1c1
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-08 17:14:55 +0200

    InsetsEditor

--- src/components/InsetsEditor.tsx
+++ src/components/InsetsEditor.tsx
@@ -1,10 +1,11 @@
 import { Spacing } from "@/label-layout";
-import { Grid, NumberInput } from "@mantine/core";
+import { NumberInput } from "@mantine/core";
 
 
 interface Props
 {
     insets: Spacing;
+    onChange?: (insets: Spacing) => void;
 }
 
 export default function InsetsEditor(props: Props): React.ReactNode
@@ -12,16 +13,34 @@ export default function InsetsEditor(props: Props): React.ReactNode
     return (
         <div style={{ display: 'inline-grid', gridTemplateColumns: 'auto auto auto', gap: '0.5em' }}>
             <div />
-            <NumberInput min={0} step={1} value={props.insets.top} suffix='mm' w='5em'  />
+            <NumberInput min={0} step={1} value={props.insets.top} suffix='mm' w='5em' onChange={onChange('top')} />
             <div />
 
-            <NumberInput min={0} step={1} value={props.insets.left} suffix='mm' w='5em' />
+            <NumberInput min={0} step={1} value={props.insets.left} suffix='mm' w='5em' onChange={onChange('left')} />
             <div />
-            <NumberInput min={0} step={1} value={props.insets.right} suffix='mm' w='5em' />
+            <NumberInput min={0} step={1} value={props.insets.right} suffix='mm' w='5em' onChange={onChange('right')} />
 
             <div />
-            <NumberInput min={0} step={1} value={props.insets.bottom} suffix='mm' w='5em' />
+            <NumberInput min={0} step={1} value={props.insets.bottom} suffix='mm' w='5em' onChange={onChange('bottom')} />
             <div />
         </div>
     );
+
+
+    function onChange(name: keyof Spacing): (value: number | string) => void
+    {
+        return (value: number | string) =>
+        {
+            if (typeof value === 'string')
+            {
+                console.error(`Unexpected string value: ${value}`);
+                return;
+            }
+
+            props.onChange?.({
+                ...props.insets,
+                [name]: value
+            });
+        };
+    }
 }
