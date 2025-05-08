b300da1fa0d19afd5931a8371e65fbe366cd51f1
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-08 17:15:32 +0200

    generate labels page update

--- src/pages/seller/GenerateLabelsSubpage.tsx
+++ src/pages/seller/GenerateLabelsSubpage.tsx
@@ -1,6 +1,10 @@
+import CaptionedBox from "@/components/CaptionedBox";
+import InsetsEditor from "@/components/InsetsEditor";
 import LabelLayoutViewer from "@/components/LabelLayoutViewer";
+import NumberInput from "@/components/NumberInput";
 import { LabelLayoutData } from "@/label-layout";
-import { NumberInput, Stack } from "@mantine/core";
+import { Box, Flex, Grid, Group, Stack, Table } from "@mantine/core";
+import { useState } from "react";
 
 
 interface Props
@@ -10,7 +14,7 @@ interface Props
 
 export default function GenerateLabelsSubpage(props: Props): React.ReactNode
 {
-    const labelLayout: LabelLayoutData = {
+    const defaultLabelLayout: LabelLayoutData = {
         paperWidth: 210,
         paperHeight: 297,
         paperMargins: {
@@ -36,20 +40,43 @@ export default function GenerateLabelsSubpage(props: Props): React.ReactNode
         fontSize: 12
     };
 
+    const [labelLayout, setLabelLayout] = useState<LabelLayoutData>(defaultLabelLayout);
+
     return (
-        <Stack justify="flex-start" align="stretch">
-            <LabelLayoutViewer width={500} labelLayout={labelLayout} />
-            <NumberInput label="Paper Width" min={0} step={1} />
-            <NumberInput label="Paper Height" min={0} step={1} />
-            <NumberInput label="Top Margin" min={0} step={1} />
-            <NumberInput label="Bottom Margin" min={0} step={1} />
-            <NumberInput label="Left Margin" min={0} step={1} />
-            <NumberInput label="Right Margin" min={0} step={1} />
-            <NumberInput label="Column Count" min={0} step={1} />
-            <NumberInput label="Row Count" min={0} step={1} />
-            <NumberInput label="Label Margin" min={0} step={1} />
-            <NumberInput label="Label Padding" min={0} step={1} />
-            <NumberInput label="Font Size" min={0} step={1} />
+        <Stack align="center">
+            <LabelLayoutViewer width='400' labelLayout={labelLayout} />
+            <Stack align="stretch" w='300px'>
+                <CaptionedBox caption="Label Counts">
+                    <Stack>
+                        <NumberInput label="Columns" min={1} step={1} value={labelLayout.columns} onChange={onChange('columns')} />
+                        <NumberInput label="Rows" min={1} step={1} value={labelLayout.rows} onChange={onChange('rows')} />
+                    </Stack>
+                </CaptionedBox>
+                <CaptionedBox caption="Paper Size">
+                    <Stack>
+                        <NumberInput label="Width" min={0} step={1} value={labelLayout.paperWidth} suffix='mm' onChange={onChange('paperWidth')} />
+                        <NumberInput label="Height" min={0} step={1} value={labelLayout.paperHeight} suffix='mm' onChange={onChange('paperHeight')} />
+                    </Stack>
+                </CaptionedBox>
+                <CaptionedBox caption="Paper Margins">
+                    <InsetsEditor insets={labelLayout.paperMargins} onChange={onChange('paperMargins')} />
+                </CaptionedBox>
+                <CaptionedBox caption="Label Margins">
+                    <InsetsEditor insets={labelLayout.labelMargins} onChange={onChange('labelMargins')} />
+                </CaptionedBox>
+                <CaptionedBox caption="Label Padding">
+                    <InsetsEditor insets={labelLayout.labelPadding} onChange={onChange('labelPadding')} />
+                </CaptionedBox>
+            </Stack>
         </Stack>
     );
+
+
+    function onChange<K extends keyof LabelLayoutData>(key: K): (value: LabelLayoutData[K]) => void
+    {
+        return (value: LabelLayoutData[K]) =>
+        {
+            setLabelLayout((prev) => ({ ...prev, [key]: value }));
+        };
+    }
 }
