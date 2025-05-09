8f962847a2d55bb048e7a28b3cc71a751747e8e1
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-09 13:09:04 +0200

    generate labels page

--- src/pages/seller/GenerateLabelsSubpage.tsx
+++ src/pages/seller/GenerateLabelsSubpage.tsx
@@ -1,10 +1,16 @@
-import CaptionedBox from "@/components/CaptionedBox";
 import InsetsEditor from "@/components/InsetsEditor";
+import ItemsTable from "@/components/ItemsTable";
+import { categoryColumn, descriptionColumn, itemIdColumn, selectionColumn } from "@/components/ItemsTable/columns";
 import LabelLayoutViewer from "@/components/LabelLayoutViewer";
+import Loading from "@/components/Loading";
 import NumberInput from "@/components/NumberInput";
 import { LabelLayoutData } from "@/label-layout";
-import { Box, Flex, Grid, Group, Stack, Table } from "@mantine/core";
-import { useState } from "react";
+import { generateLabels } from "@/rest/generate-labels";
+import { Item } from "@/rest/item-data";
+import { listSellerItems } from "@/rest/list-seller-items";
+import { RestStatus } from "@/rest/status";
+import { Button, Center, Flex, Group, Stack, Stepper, Tabs, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 
 interface Props
@@ -40,43 +46,160 @@ export default function GenerateLabelsSubpage(props: Props): React.ReactNode
         fontSize: 12
     };
 
+    const [itemsStatus, setItemsStatus] = useState<RestStatus<Item[]>>({ status: 'loading' });
     const [labelLayout, setLabelLayout] = useState<LabelLayoutData>(defaultLabelLayout);
+    const [selectedItems, setSelectedItems] = useState<{ [id: string]: boolean }>({});
+    useEffect(() => {
+            void (async () => {
+                const response = await listSellerItems(props.sellerId);
 
-    return (
-        <Stack align="center">
-            <LabelLayoutViewer width='400' labelLayout={labelLayout} />
-            <Stack align="stretch" w='300px'>
-                <CaptionedBox caption="Label Counts">
-                    <Stack>
-                        <NumberInput label="Columns" min={1} step={1} value={labelLayout.columns} onChange={onChange('columns')} />
-                        <NumberInput label="Rows" min={1} step={1} value={labelLayout.rows} onChange={onChange('rows')} />
-                    </Stack>
-                </CaptionedBox>
-                <CaptionedBox caption="Paper Size">
-                    <Stack>
-                        <NumberInput label="Width" min={0} step={1} value={labelLayout.paperWidth} suffix='mm' onChange={onChange('paperWidth')} />
-                        <NumberInput label="Height" min={0} step={1} value={labelLayout.paperHeight} suffix='mm' onChange={onChange('paperHeight')} />
-                    </Stack>
-                </CaptionedBox>
-                <CaptionedBox caption="Paper Margins">
-                    <InsetsEditor insets={labelLayout.paperMargins} onChange={onChange('paperMargins')} />
-                </CaptionedBox>
-                <CaptionedBox caption="Label Margins">
-                    <InsetsEditor insets={labelLayout.labelMargins} onChange={onChange('labelMargins')} />
-                </CaptionedBox>
-                <CaptionedBox caption="Label Padding">
-                    <InsetsEditor insets={labelLayout.labelPadding} onChange={onChange('labelPadding')} />
-                </CaptionedBox>
-            </Stack>
-        </Stack>
-    );
+                if (response.success)
+                {
+                    setItemsStatus({ status: 'success', value: response.value.items });
 
+                    const mapping: { [id: string]: boolean } = {};
+                    for (const item of response.value.items)
+                    {
+                        mapping[item.itemId] = true;
+                    }
+                    setSelectedItems(mapping);
+                }
+                else
+                {
+                    setItemsStatus({ status: 'error', tag: response.error.type, details: response.error.details });
+                }
+            })();
+        }, [props.sellerId]);
 
-    function onChange<K extends keyof LabelLayoutData>(key: K): (value: LabelLayoutData[K]) => void
+    switch (itemsStatus.status)
     {
-        return (value: LabelLayoutData[K]) =>
+        case 'success':
+            return renderPage(itemsStatus.value);
+
+        case 'loading':
+            return (
+                <Loading message="Loading items..." />
+            );
+
+        case 'error':
+            return <div>Error: {itemsStatus.details}</div>;
+    }
+
+
+    function renderPage(items: Item[]): React.ReactNode
+    {
+        const columns = [
+            selectionColumn(item => selectedItems[item.itemId] === true, (item, selected) => { setSelectedItems({...selectedItems, [item.itemId]: selected}) }),
+            itemIdColumn,
+            descriptionColumn,
+            categoryColumn,
+        ]
+
+        return (
+            <Stepper active={1} color="teal" size="sm" iconPosition="right" style={{ width: '100%' }}>
+                <Stepper.Step label="Select Items" allowStepSelect={false}>
+                    <ItemsTable items={items} columns={columns} />
+                </Stepper.Step>
+                <Stepper.Step label="Label Layout" allowStepSelect={false}>
+                    <Group justify="center" align="flex-start">
+                        <LabelLayoutViewer width='400' labelLayout={labelLayout} />
+                        <Stack align="stretch">
+                            <Tabs defaultValue="label-counts">
+                                <Tabs.List>
+                                    <Tabs.Tab value="label-counts">Label Counts</Tabs.Tab>
+                                    <Tabs.Tab value="paper-size">Sheet Size</Tabs.Tab>
+                                    <Tabs.Tab value="paper-margins">Sheet Margins</Tabs.Tab>
+                                    <Tabs.Tab value="label-margins">Label Margins</Tabs.Tab>
+                                </Tabs.List>
+                                <Tabs.Panel value="label-counts">
+                                    <Stack mt='md'>
+                                        <Flex justify='stretch'>
+                                            <Text>
+                                                Here you can set the number of labels per row and column.
+                                            </Text>
+                                        </Flex>
+                                        <NumberInput label="Columns" min={1} step={1} value={labelLayout.columns} onChange={onChange('columns')} />
+                                        <NumberInput label="Rows" min={1} step={1} value={labelLayout.rows} onChange={onChange('rows')} />
+                                    </Stack>
+                                </Tabs.Panel>
+                                <Tabs.Panel value="paper-size">
+                                    <Stack mt='md'>
+                                        <Flex justify='stretch'>
+                                            <Text w='400px'>
+                                                Here you can set the size of the paper. The default is A4 (210mm x 297mm).
+                                                Note that the preview might react unintuitively to the changes:
+                                                it accurately reflects the proportions of the paper, but not the actual size.
+                                            </Text>
+                                        </Flex>
+                                        <NumberInput label="Width" min={0} step={1} value={labelLayout.paperWidth} suffix='mm' onChange={onChange('paperWidth')} />
+                                        <NumberInput label="Height" min={0} step={1} value={labelLayout.paperHeight} suffix='mm' onChange={onChange('paperHeight')} />
+                                    </Stack>
+                                </Tabs.Panel>
+                                <Tabs.Panel value="paper-margins">
+                                    <Stack mt='md'>
+                                        <Flex justify='stretch'>
+                                            <Text w='400px'>
+                                                Here you can set the size of the margins of the paper.
+                                            </Text>
+                                        </Flex>
+                                        <Center>
+                                            <InsetsEditor insets={labelLayout.paperMargins} onChange={onChange('paperMargins')} />
+                                        </Center>
+                                    </Stack>
+                                </Tabs.Panel>
+                                <Tabs.Panel value="label-margins">
+                                    <Stack mt='md'>
+                                        <Flex justify='stretch'>
+                                            <Text w='400px'>
+                                                Here you can set the size of the paper. The default is A4 (210mm x 297mm).
+                                                Note that the preview might react unintuitively to the changes:
+                                                it accurately reflects the proportions of the paper, but not the actual size.
+                                            </Text>
+                                        </Flex>
+                                        <Center>
+                                            <InsetsEditor insets={labelLayout.labelMargins} onChange={onChange('labelMargins')} />
+                                        </Center>
+                                    </Stack>
+                                </Tabs.Panel>
+                            </Tabs>
+                        </Stack>
+                    </Group>
+                </Stepper.Step>
+                <Stepper.Step label="Generate PDF" allowStepSelect={false}>
+                    <Button onClick={onGenerateLabels} disabled={items.length === 0}>Generate</Button>
+                </Stepper.Step>
+            </Stepper>
+        );
+
+
+        function onChange<K extends keyof LabelLayoutData>(key: K): (value: LabelLayoutData[K]) => void
         {
-            setLabelLayout((prev) => ({ ...prev, [key]: value }));
-        };
+            return (value: LabelLayoutData[K]) =>
+            {
+                setLabelLayout((prev) => ({ ...prev, [key]: value }));
+            };
+        }
+
+        function onGenerateLabels(): void
+        {
+            void (async () => {
+                const payload = { layout: labelLayout, itemIds: items.map(item => item.itemId) };
+                const blob = await generateLabels(payload);
+
+                if ( !blob.success )
+                {
+                    console.error("Failed to generate labels", blob.error);
+                    return;
+                }
+
+                const url = window.URL.createObjectURL(blob.value);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = 'labels.pdf';
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+            })();
+        }
     }
 }
