be34825b7416ac653311723d03d8f4443964597d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-05-10 23:14:40 +0200

    updated CaptionedBox

--- src/components/CaptionedBox.tsx
+++ src/components/CaptionedBox.tsx
@@ -1,30 +1,47 @@
 import { Collapse, Group, Stack, Text } from '@mantine/core';
-import classes from './CaptionedBox.module.css';
+import { IconCircleChevronDown } from '@tabler/icons-react';
 import { useState } from 'react';
-import { IconChevronDown, IconCircleChevronDown } from '@tabler/icons-react';
+import classes from './CaptionedBox.module.css';
 
 
 interface Props
 {
     caption: string;
     children: React.ReactNode;
+    collapsable?: boolean;
 }
 
 export default function CaptionedBox(props: Props): React.ReactNode
 {
     const [collapsed, setCollapsed] = useState(false);
+    const isCollapsed = collapsed || !props.collapsable;
 
     return (
         <Stack className={classes.container} gap={0}>
             <Group className={classes.captionContainer} onClick={() => setCollapsed(!collapsed)} justify='stretch' style={{position: 'relative'}}>
-                <IconCircleChevronDown style={{position: 'absolute', right: '2px', height: '100%'}} />
+                {renderCollapseIcon()}
                 <Text className={classes.caption}>{props.caption}</Text>
             </Group>
-            <Collapse in={collapsed} transitionDuration={250}>
+            <Collapse in={isCollapsed} transitionDuration={250}>
                 <div className={classes.childContainer}>
                     {props.children}
                 </div>
             </Collapse>
         </Stack>
     );
+
+
+    function renderCollapseIcon(): React.ReactNode
+    {
+        if (props.collapsable)
+        {
+            return (
+                <IconCircleChevronDown style={{position: 'absolute', right: '2px', height: '100%'}} />
+            );
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
