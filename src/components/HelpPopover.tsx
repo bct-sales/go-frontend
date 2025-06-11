f4e686ee7a79c19e793eddca75c4d68e5f942e82
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-11 13:06:19 +0200

    improved tabbing

--- src/components/HelpPopover.tsx
+++ src/components/HelpPopover.tsx
@@ -11,7 +11,7 @@ export default function HelpPopover(props: Props): React.ReactNode
     return (
         <Popover position="bottom" shadow="md" width={250}>
             <Popover.Target>
-                <Button variant="subtle" size="xs" p={0}>
+                <Button variant="subtle" size="xs" p={0} tabIndex={-1}>
                     <IconHelpCircle />
                 </Button>
             </Popover.Target>
