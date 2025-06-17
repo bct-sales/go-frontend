5e037d00fc9f3061a6ddc0475c7b6495c6704b8c
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-06-17 14:01:42 +0200

    refactoring

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -4,6 +4,7 @@ import DateTimeViewer from "./DateTimeViewer";
 import { NavLink } from "react-router-dom";
 import { DateTime } from "@/datetime";
 import UserIdViewer from "./UserIdViewer";
+import { DataTable } from "mantine-datatable";
 
 interface Props
 {
@@ -25,49 +26,41 @@ export default function UsersTable(props: Props): React.ReactNode
     const { users } = props;
 
     return (
-        <Table className={classes.userTable}>
-            <Table.Thead>
-                <Table.Tr>
-                    <Table.Th>Id</Table.Th>
-                    <Table.Th>Role</Table.Th>
-                    <Table.Th>Item Count</Table.Th>
-                    <Table.Th>Created At</Table.Th>
-                    <Table.Th>Last Activity</Table.Th>
-                    <Table.Th>Password</Table.Th>
-                </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-                {users.map(renderUser)}
-            </Table.Tbody>
-        </Table>
+        <DataTable
+            striped
+            records={users}
+            columns={[
+                {
+                    accessor: 'id',
+                    title: 'Id',
+                    render: (user: User) => (
+                        <NavLink to={`/admin/users/${user.id}`}>
+                            <UserIdViewer userId={user.id} />
+                        </NavLink>
+                    ),
+                },
+                {
+                    accessor: 'role',
+                    title: 'Role'
+                },
+                {
+                    accessor: 'itemCount',
+                    title: 'Item Count'
+                },
+                {
+                    accessor: 'createdAt',
+                    title: 'Created At',
+                    render: (user: User) => <DateTimeViewer dateTime={user.createdAt} />,
+                },
+                {
+                    accessor: 'lastActivity',
+                    title: 'Last Activity',
+                    render: (user: User) => renderLastActivity(user.lastActivity),
+                },
+                { accessor: 'password', title: 'Password' },
+            ]}
+        />
     );
-
-
-    function renderUser(user: User): React.ReactNode
-    {
-        return (
-            <Table.Tr key={user.id} className={classes.userRow}>
-                <Table.Td className={classes.userData}>
-                    <UserIdViewer userId={user.id} />
-                </Table.Td>
-                <Table.Td className={classes.userData}>
-                    {user.role}
-                </Table.Td>
-                <Table.Td className={classes.userData}>
-                    {user.itemCount}
-                </Table.Td>
-                <Table.Td className={classes.userData}>
-                    <DateTimeViewer dateTime={user.createdAt} />
-                </Table.Td>
-                <Table.Td className={classes.userData}>
-                    {renderLastActivity(user.lastActivity)}
-                </Table.Td>
-                <Table.Td className={classes.userData}>
-                    {user.password}
-                </Table.Td>
-            </Table.Tr>
-        );
-    }
 
 
     function renderLastActivity(lastActivity: User['lastActivity']): React.ReactNode
