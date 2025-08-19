3076d0f6c6ab93e1fbb0357963bc595f0df39a68
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-19 13:03:01 +0200

    seller summary styling

--- src/components/SellerSummaryViewer.tsx
+++ src/components/SellerSummaryViewer.tsx
@@ -1,6 +1,6 @@
 import Price from "@/components/Price";
 import { Table } from "@mantine/core";
-
+import classes from './SellerSummaryViewer.module.css';
 
 interface Props
 {
@@ -15,16 +15,16 @@ export default function SellerSummaryViewer(props: Props) : React.ReactNode
         <Table variant="vertical">
             <Table.Tbody>
                 <Table.Tr>
-                    <Table.Th>Item Count</Table.Th>
-                    <Table.Td>{props.itemCount}</Table.Td>
+                    <Table.Th className={classes.header}>Item Count</Table.Th>
+                    <Table.Td className={classes.value}>{props.itemCount}</Table.Td>
                 </Table.Tr>
                 <Table.Tr>
-                    <Table.Th>Unlabeled Items</Table.Th>
-                    <Table.Td>{props.itemCount - props.frozenItemCount}</Table.Td>
+                    <Table.Th className={classes.header}>Unlabeled Items</Table.Th>
+                    <Table.Td className={classes.value}>{props.itemCount - props.frozenItemCount}</Table.Td>
                 </Table.Tr>
                 <Table.Tr>
-                    <Table.Th>Total Item's Worth</Table.Th>
-                    <Table.Td><Price priceInCents={props.totalPrice} /></Table.Td>
+                    <Table.Th className={classes.header}>Total Item's Worth</Table.Th>
+                    <Table.Td className={classes.value}><Price priceInCents={props.totalPrice} /></Table.Td>
                 </Table.Tr>
             </Table.Tbody>
         </Table>
