513da84390684a54a65f4e8eaaf7a17e64bb1904
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-23 14:04:40 +0200

    updated donation label

--- src/components/DonationEditor.tsx
+++ src/components/DonationEditor.tsx
@@ -13,7 +13,7 @@ export default function DonationEditor(props: Props): React.ReactNode
 {
     return (
         <Flex justify="space-between">
-            <Checkbox icon={DonationCheckboxIcon} checked={props.donation} onChange={onChange} label="Donation" size="lg" />
+            <Checkbox icon={DonationCheckboxIcon} checked={props.donation} onChange={onChange} label="Donate proceeds" size="lg" />
             <HelpPopover>
                 In case the item gets sold, the profits will be given to the BCT.
             </HelpPopover>
