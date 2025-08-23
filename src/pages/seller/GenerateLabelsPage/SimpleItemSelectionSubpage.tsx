f3f7075da9f6b8ae47f57636dd663fce8e41f64d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-23 15:57:36 +0200

    layout

--- src/pages/seller/GenerateLabelsPage/SimpleItemSelectionSubpage.tsx
+++ src/pages/seller/GenerateLabelsPage/SimpleItemSelectionSubpage.tsx
@@ -1,7 +1,7 @@
 import ItemsTable from "@/components/ItemsTable";
 import { categoryColumn, countColumn, descriptionColumn, donationColumn, itemIdColumn, priceInCentsColumn } from "@/components/ItemsTable/columns";
 import { Item } from "@/components/ItemsTable/ItemsTable";
-import { Button, Stack, Text } from "@mantine/core";
+import { Button, Card, Center, Stack, Text } from "@mantine/core";
 
 interface Props
 {
@@ -25,22 +25,27 @@ export default function SimpleItemSelectionSubpage(props: Props): React.ReactNod
 
     return (
         <Stack>
-            {renderExplanations()}
             <ItemsTable items={items} columns={columns} />
-            <Button onClick={props.goToNextStep}>Next Step</Button>
+            <Center>
+                <Button onClick={props.goToNextStep} w='50%'>Next Step</Button>
+            </Center>
         </Stack>
     );
 
 
     function renderExplanations(): React.ReactNode
     {
-        return <></>;
-        // return (
-        //     <Text>
-        //         Here you can specify how many labels you want to print for each item.
-        //         We strongly recommend you print at least two labels for each item.
-        //         If the item consists of multiple disjoint parts, you should print a label for each part.
-        //     </Text>
-        // );
+        return (
+            <Center>
+                <Card w='50%' m='xl'>
+                    <Text>
+                        Below is a list of all items you entered. You can choose how many labels you wish to print for each.
+                        We strongly recommend printing two labels per item.
+                        If an item consists of multiple disjoint parts, you should print out one label per part.
+                        This helps on the day of the sale in case a label gets accidentally detached, or parts of the same item get separated.
+                    </Text>
+                </Card>
+            </Center>
+        );
     }
 }
