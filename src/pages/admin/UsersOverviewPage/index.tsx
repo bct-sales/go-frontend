c9737aab5e95a6f94277c86c1f990e1298a93b49
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-08-29 19:27:50 +0200

    downloadable user data

--- src/pages/admin/UsersOverviewPage/index.tsx
+++ src/pages/admin/UsersOverviewPage/index.tsx
@@ -1,7 +1,10 @@
+import DownloadAs from "@/components/DownloadAs";
 import Loading from "@/components/Loading";
 import UsersTable from "@/components/UsersTable";
 import { listUsers, User } from "@/rest/list-users";
+import { paths } from "@/rest/paths";
 import { RestStatus } from "@/rest/status";
+import { Group, Stack } from "@mantine/core";
 import { useEffect, useState } from "react";
 
 
@@ -43,7 +46,12 @@ export default function UsersOverviewPage()
     {
         return (
             <>
-                <UsersTable users={users} />
+                <Stack>
+                    <Group justify="flex-end">
+                        <DownloadAs cvsUrl={paths.usersAsCsv} jsonUrl={paths.usersAsJson} />
+                    </Group>
+                    <UsersTable users={users} />
+                </Stack>
             </>
         );
     }
