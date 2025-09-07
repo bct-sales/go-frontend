b9062724b73ec97e964d0ea2685f4faabe267b8f
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-09-07 18:08:04 +0200

    updated donation editor

--- src/components/DonationEditor.tsx
+++ src/components/DonationEditor.tsx
@@ -13,7 +13,7 @@ export default function DonationEditor(props: Props): React.ReactNode
 {
     return (
         <Flex justify="space-between">
-            <Checkbox icon={DonationCheckboxIcon} checked={props.donation} onChange={onChange} label="Donate proceeds" size="lg" />
+            <Checkbox icon={DonationCheckboxIcon} checked={props.donation} onChange={onChange} label="Donate &euro; to BCT" size="lg" />
             <HelpPopover>
                 In case the item gets sold, the proceeds will be donated to the BCT.
                 If the item does not get sold, the item returns back to you.
