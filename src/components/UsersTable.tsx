4bf8e7fb79c536d445fb6f36fbb7b0d2ecbe5952
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-09-10 00:15:36 +0200

    user table pagination

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -5,6 +5,7 @@ import DateTimeViewer from "./DateTimeViewer";
 import UserIdViewer from "./UserIdViewer";
 import { useEffect, useState } from "react";
 import sortBy from 'lodash/sortBy';
+import { Pagination, Stack } from "@mantine/core";
 
 interface Props
 {
@@ -28,7 +29,10 @@ export default function UsersTable(props: Props): React.ReactNode
         columnAccessor: 'id',
         direction: 'asc',
     });
-    const [rows, setRows] = useState(sortBy(users, 'id'));
+    const [ rows, setRows ] = useState(sortBy(users, 'id'));
+    const [ page, setPage ] = useState(1);
+    const rowsPerPage = 20;
+    const lastPage = Math.ceil(users.length / rowsPerPage);
 
     useEffect(() => {
         const sortedUsers = sortBy(users, sortStatus.columnAccessor);
@@ -39,50 +43,55 @@ export default function UsersTable(props: Props): React.ReactNode
         setRows(sortedUsers);
     }, [users, sortStatus]);
 
+    const rowsOnPage = rows.slice((page - 1) * rowsPerPage, page * rowsPerPage);
+
     return (
-        <DataTable
-            striped
-            records={rows}
-            highlightOnHover
-            height="calc(100vh - 200px)"
-            columns={[
-                {
-                    accessor: 'id',
-                    title: 'Id',
-                    render: (user: User) => (
-                        <NavLink to={`/admin/users/${user.id}`}>
-                            <UserIdViewer userId={user.id} />
-                        </NavLink>
-                    ),
-                    sortable: true,
-                },
-                {
-                    accessor: 'role',
-                    title: 'Role'
-                },
-                {
-                    accessor: 'itemCount',
-                    title: 'Item Count',
-                    sortable: true,
-                },
-                {
-                    accessor: 'createdAt',
-                    title: 'Created At',
-                    render: (user: User) => <DateTimeViewer dateTime={user.createdAt} />,
-                },
-                {
-                    accessor: 'lastActivity',
-                    title: 'Last Activity',
-                    render: (user: User) => renderLastActivity(user.lastActivity),
-                },
-                {
-                    accessor: 'password',
-                    title: 'Password'
-                },
-            ]}
-            sortStatus={sortStatus}
-            onSortStatusChange={setSortStatus}
-        />
+        <Stack>
+            <Pagination value={page} onChange={setPage} total={lastPage} />
+            <DataTable
+                striped
+                records={rowsOnPage}
+                highlightOnHover
+                height="calc(100vh - 200px)"
+                columns={[
+                    {
+                        accessor: 'id',
+                        title: 'Id',
+                        render: (user: User) => (
+                            <NavLink to={`/admin/users/${user.id}`}>
+                                <UserIdViewer userId={user.id} />
+                            </NavLink>
+                        ),
+                        sortable: true,
+                    },
+                    {
+                        accessor: 'role',
+                        title: 'Role'
+                    },
+                    {
+                        accessor: 'itemCount',
+                        title: 'Item Count',
+                        sortable: true,
+                    },
+                    {
+                        accessor: 'createdAt',
+                        title: 'Created At',
+                        render: (user: User) => <DateTimeViewer dateTime={user.createdAt} />,
+                    },
+                    {
+                        accessor: 'lastActivity',
+                        title: 'Last Activity',
+                        render: (user: User) => renderLastActivity(user.lastActivity),
+                    },
+                    {
+                        accessor: 'password',
+                        title: 'Password'
+                    },
+                ]}
+                sortStatus={sortStatus}
+                onSortStatusChange={setSortStatus}
+            />
+        </Stack>
     );
 
 
