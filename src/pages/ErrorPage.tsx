b2e153ba0d0485a83ee856e5523e8555ca109f9c
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-09-29 16:43:25 +0200

    ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,9 @@
 import { useAuthentication } from "@/authentication";
 import { Alert, Button, Stack } from "@mantine/core";
+import { IconAlertTriangle } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
+import classes from './ErrorPage.module.css';
+
 
 interface Props
 {
@@ -11,12 +14,14 @@ export default function ErrorPage(props: Props): React.ReactNode
 {
     const authentication = useAuthentication();
     const navigate = useNavigate();
+    const alertIcon = <IconAlertTriangle />;
 
     return (
         <Stack align="center" justify="center">
-            <Alert title="Error" color="red">
-                {props.children}
+            <Alert icon={alertIcon} color='red' title="An error occurred" w='50%' className={classes.alert}>
+                Please contact the site administrator and provide them with the information shown below.
             </Alert>
+            {props.children}
             <Button onClick={onLogout} color="red">
                 Go back to login page
             </Button>
