41f4c9cbc6d64680161e3feb2ce756d52cdf640b
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-02 20:23:25 +0200

    added idAccessor in CategoryCountsTable

--- src/components/CategoryCountsTable.tsx
+++ src/components/CategoryCountsTable.tsx
@@ -24,6 +24,7 @@ export default function CategoryCountsTable(props: Props): React.ReactNode
             highlightOnHover
             striped
             records={categoryCounts}
+            idAccessor="categoryId"
             columns={[
                 {
                     accessor: 'categoryId',
