a1aa7fe3d0a06d6f4b3c97b9d89c79fd93a0507d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-02 16:20:13 +0200

    removed unused function

--- src/pages/seller/GenerateLabelsPage/SimpleItemSelectionSubpage.tsx
+++ src/pages/seller/GenerateLabelsPage/SimpleItemSelectionSubpage.tsx
@@ -31,23 +31,4 @@ export default function SimpleItemSelectionSubpage(props: Props): React.ReactNod
             </Center>
         </Stack>
     );
-
-
-    function renderExplanations(): React.ReactNode
-    {
-        return (
-            <Center>
-                <Card w='50%' m='xl'>
-                    <Text>
-                        Below is a list of all items you entered. You can choose how many labels you wish to print for each.
-                        We strongly recommend printing two labels per item.
-                        If an item consists of multiple disjoint parts, you should print out one label per part.
-                        This helps on the day of the sale in case a label gets accidentally detached, or parts of the same item get separated.
-                    </Text>
-                </Card>
-            </Center>
-        );
-    }
 }
-
-\ No newline at end of file
