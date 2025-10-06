e5ba7ba4e51f1d03658c408dbaad0edbab45873a
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-06 14:04:41 +0200

    ExportButton

---
+++ src/components/ExportButton.tsx
@@ -0,0 +1,43 @@
+import { Button, Menu } from "@mantine/core";
+import { IconDownload } from "@tabler/icons-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+interface ExportFormat
+{
+    caption: string;
+    url: string;
+    filename: string;
+}
+
+interface Props
+{
+    formats: ExportFormat[];
+}
+
+export default function ExportButton(props: Props): React.ReactNode
+{
+    return (
+        <Menu>
+            <Menu.Target>
+                <Button variant="outline">
+                    <IconDownload size={16} stroke={1.5} />
+                </Button>
+            </Menu.Target>
+            <Menu.Dropdown>
+                {props.formats.map(renderFormatMenuItem)}                
+            </Menu.Dropdown>
+        </Menu>
+    );
+
+
+    function renderFormatMenuItem(format: ExportFormat): React.ReactNode
+    {
+        return (
+            <Menu.Item component={Link} to={format.url} target="_blank" download={format.filename}>
+                {format.caption}
+            </Menu.Item>
+        );
+    }
+}
