ae433767c00348ce9ed445c5316d114924cae994
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-10-22 16:29:52 +0200

    LargeItemCheckboxIcon

---
+++ src/components/LargeItemCheckboxIcon.tsx
@@ -0,0 +1,15 @@
+import { IconArrowsMaximize } from "@tabler/icons-react";
+
+
+interface Props
+{
+    indeterminate?: boolean;
+    className?: string;
+}
+
+export default function LargeItemCheckboxIcon(props: Props): React.ReactNode
+{
+    return (
+        <IconArrowsMaximize className={props.className} />
+    );
+}
