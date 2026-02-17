4dc0e488d4051e0d57c34b6b45d308aa6b4fce9c
Frederic Vogels <frederic.vogels@gmail.com>
2026-02-17 22:24:31 +0100

    reststatus update

--- src/rest/status.ts
+++ src/rest/status.ts
@@ -1,6 +1,6 @@
 import { ErrorTag } from "./result";
 
 export type RestStatus<T> =
-    | { status: 'loading' }
-    | { status: 'error'; tag: ErrorTag; details: string }
-    | { status: 'success'; value: T };
+    | { readonly status: 'loading' }
+    | { readonly status: 'error'; readonly tag: ErrorTag; readonly details: string }
+    | { readonly status: 'success'; readonly value: T };
