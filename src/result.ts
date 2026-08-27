876e9779ea9f0ca1e0da9ac60164ef90ec677804
Frederic Vogels <frederic.vogels@gmail.com>
2026-08-27 21:13:09 +0200

    result functionality

--- src/result.ts
+++ src/result.ts
@@ -17,3 +17,13 @@ export function success<T>(value: T): Success<T> {
 export function failure<T>(error: T): Failure<T> {
     return { success: false, error };
 }
+
+export function is_success<T, E>(result: Result<T, E>): result is Success<T> {
+    return result.success;
+}
+
+export function assert_success<T, E>(result: Result<T, E>): asserts result is Success<T> {
+    if ( !is_success(result) ) {
+        throw new Error("failure");
+    }
+}
